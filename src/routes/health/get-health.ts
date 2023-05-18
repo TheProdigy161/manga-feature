92d3fdfa287892517a1f65e1a8968efe7c107f7b
TheProdigy161 <TheProdigy161@yahoo.co.uk>
2023-05-18 20:52:36 +0100

    Updated get health message

--- src/routes/health/get-health.ts
+++ src/routes/health/get-health.ts
@@ -1,9 +1,5 @@
 import { Response, Request } from 'express';
 
 export default (_req: Request, res: Response) => {
-    try {
-        res.send({ message: "Hahahahah" });
-    } catch (error) {
-        console.log('Failed to get');
-    }
+    res.send({ message: "Api is running." });
 }
